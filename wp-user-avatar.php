014a17de74dcd1d01f8091c3f185af074f492d93
bangbay <bangbay@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-22 02:14:01 +0000

    Update readme.txt.
    git-svn-id: https://plugins.svn.wordpress.org/wp-user-avatar/trunk@842809 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-user-avatar.php
+++ wp-user-avatar.php
@@ -366,7 +366,7 @@ if(!class_exists('wp_user_avatar')){
       }
     }
     // Add to edit user profile
-    function wpua_action_show_user_profile($user){
+    public static function wpua_action_show_user_profile($user){
       global $blog_id, $current_user, $post, $show_avatars, $wpdb, $wpua_allow_upload, $wpua_edit_avatar, $wpua_upload_size_limit_with_units;
       // Get WPUA attachment ID
       $wpua = get_user_meta($user->ID, $wpdb->get_blog_prefix($blog_id).'user_avatar', true);
@@ -554,13 +554,13 @@ if(!class_exists('wp_user_avatar')){
     }
 
     // Media uploader
-    function wpua_media_upload_scripts($user=""){
+    public static function wpua_media_upload_scripts($user=""){
       global $mustache_admin, $pagenow, $show_avatars, $wpua_upload_size_limit;
       wp_enqueue_script('jquery');
       if(current_user_can('upload_files')){
         wp_enqueue_script('admin-bar');
         wp_enqueue_media();
-        wp_enqueue_script('wp-user-avatar', WPUA_URLPATH.'js/wp-user-avatar.js', array('jquery'), WPUA_VERSION, true);
+        wp_enqueue_script('wp-user-avatar', WPUA_URLPATH.'js/wp-user-avatar.js', array('jquery','media-editor'), WPUA_VERSION, true);
       } else {
         wp_enqueue_script('wp-user-avatar', WPUA_URLPATH.'js/wp-user-avatar-user.js', array('jquery'), WPUA_VERSION, true);
       }
